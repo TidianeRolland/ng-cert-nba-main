03883ba69d54900486bd9e26a320b00e5ce4fa56
Tidiane <rtidiane@gmail.com>
2023-04-11 17:06:29 +0200

    feat: add reusable confirm modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { TeamStatsComponent } from './team-stats/team-stats.component';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { GameResultsComponent } from './game-results/game-results.component';
 import { GameStatsComponent } from './game-stats/game-stats.component';
+import { ModalComponent } from './ui/confirm-modal/modal/modal.component';
+import { ConfirmModalModule } from './ui/confirm-modal/confirm-modal.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     TeamStatsComponent,
     GameResultsComponent,
-    GameStatsComponent
+    GameStatsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ConfirmModalModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
